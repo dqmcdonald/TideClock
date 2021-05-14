622f6afd8fe0051fde18869bd4c37eda733b07f3
Quentin McDonald <dqmcdonald@gmail.com>
2021-05-14 18:48:17 +1200

    Some time converstion expts

--- code.py
+++ code.py
@@ -13,6 +13,8 @@ import socketpool
 import ssl
 import adafruit_requests
 import json
+from adafruit_datetime import datetime, date, timezone, timedelta
+import secrets
 
 BLACK = 0x000000
 WHITE = 0xFFFFFF
@@ -26,7 +28,6 @@ BACKGROUND_COLOR = WHITE
 DISPLAY_WIDTH = 296
 DISPLAY_HEIGHT = 128
 
-PRIVATE_DATA_FILE = "private.txt"
 
 # Lyttleton Data:
 LATITUDE = -43.611
@@ -37,7 +38,7 @@ NIWA_URL = "https://api.niwa.co.nz/tides/data"
 
 def setup_display():
     """
-    Returns the display and display bus
+    Returns the display, display bus and display group
     """
     # Define the pins needed for display use
     # This pinout is for a Feather M4 and may be different for other boards
@@ -45,14 +46,11 @@ def setup_display():
     display_bus=displayio.FourWire(
         spi, command=board.D6, chip_select=board.D5,
         baudrate=1000000)
-    time.sleep(1)
-
 
     time.sleep(1)  # Wait a bit
-    print("setup display_bus")
+
+
     # Create the display object - the third color is red (0xff0000)
-
-
     display=adafruit_il0373.IL0373(
         display_bus,
         width=DISPLAY_WIDTH,
@@ -63,15 +61,9 @@ def setup_display():
         grayscale=True,
         refresh_time=1)
 
-    return(display_bus, display)
 
-def display_text(display, x, y, text):
-    """
-    Display the given text at point x, y on display
-    """
-# Create a display group for our screen objects
+    # Create a display group for our screen objects
     g = displayio.Group(max_size=10)
-    print("Setup display group")
 
     # Set a background
     background_bitmap = displayio.Bitmap(DISPLAY_WIDTH, DISPLAY_HEIGHT, 1)
@@ -83,32 +75,28 @@ def display_text(display, x, y, text):
     t = displayio.TileGrid(background_bitmap, pixel_shader=palette)
     g.append(t)
 
+    return(display_bus, display, g)
+
+def display_text(display,group, x, y, text):
+    """
+    Display the given text at point x, y on display
+    """
+
+
     # Draw simple text using the built-in font into a displayio group
     text_group = displayio.Group(max_size=10, scale=2, x=x, y=y)
     text_area = label.Label(terminalio.FONT, text=text, color=FOREGROUND_COLOR)
     text_group.append(text_area)  # Add this text to the text group
-    g.append(text_group)
+    group.append(text_group)
 
+
+def update_display(display, group ):
     # Place the display group on the screen
-    display.show(g)
-
-
+    display.show(group)
     # Refresh the display to have everything show on the display
     # NOTE: Do not refresh eInk displays more often than 180 seconds!
     display.refresh()
-
     time.sleep(180)
-
-def get_private_data(filename):
-    """
-    Read the private data from 'private.txt' and return the ssid, password anbd API key
-    """
-    with open(filename, mode='rt') as f:
-        ssid = f.readline().strip()
-        password = f.readline().strip()
-        api_key = f.readline().strip()
-
-    return (ssid, password, api_key)
 
 
 def connect_to_ssid(ssid, password):
@@ -119,7 +107,7 @@ def connect_to_ssid(ssid, password):
     try:
         wifi.radio.connect(ssid=ssid,password=password)
     except ConnectionError:
-        print("Connection to {} file file".format(ssid))
+        print("Connection to {} failed".format(ssid))
         raise
 
 
@@ -139,33 +127,48 @@ def get_tide_data(URL, apikey, lat, long):
     print("Response code:",response.status_code)
     return response.json()
 
-
-
+####### Main Routine ##################
 
 print("Tide Clock ")
 
 
-(ssid,password, api_key) = get_private_data(PRIVATE_DATA_FILE)
-print("Connecting to '{}' with pawword '{}'".format(ssid, password))
+(ssid,password, api_key) = (secrets.secrets["ssid"], secrets.secrets["password"], secrets.secrets["api_key"])
+print("Connecting to '{}' with password '{}'".format(ssid, password))
 
 connect_to_ssid(ssid, password)
 
-print("Connection to {} commplete".format( ssid ))
+print("Connection to {} complete".format( ssid ))
 print("my IP addr:", wifi.radio.ipv4_address)
 
 tide_data = get_tide_data(NIWA_URL, api_key, LATITUDE, LONGTITUDE)
 
-#print(tide_data)
+vals = tide_data["values"]
+print(vals)
+first_time = vals[1]["time"]
+first_time = first_time.replace('T', '*')
+first_time = first_time.replace('Z','')
+print(first_time)
 
-print(tide_data["values"][0]["time"])
 
-# Used to ensure the display is free in CircuitPython
+first_datetime = datetime.fromisoformat(first_time)
+UTC_offset = timedelta(hours=12)
+
+
+print( first_datetime.ctime())
+
+ft = first_datetime + UTC_offset
+print(ft.ctime(), vals[1]["value"])
+
+
 displayio.release_displays()
 
-display_bus, display = setup_display()
+display_bus, display, group = setup_display()
 
-display_text( display, 5,10, "Tide Clock")
 
+
+#display_text( display, group, 5,10, "Tide Clock - Lyttleton")
+#display_text( display, group, 5,30, "Date")
+#update_display( display, group )
 
 
 print('Done')
